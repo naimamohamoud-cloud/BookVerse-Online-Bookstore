663dfc2914ac07c05b9c014573cef96db5647608
unknown <naima.mohamoud@strathmore.edu>
2026-06-28 14:56:35 +0300

    enhanced contact form validation and succes message

--- js/script.js
+++ js/script.js
@@ -16,54 +16,74 @@ window.addEventListener("load", function () {
         let isValid = true;
 
         const name = document.getElementById("fullName").value.trim();
-        const email = document.getElementById("email").value.trim();
+        const email = document.getElementById("email").value.trim().toLowerCase();
         const subject = document.getElementById("subject").value.trim();
         const message = document.getElementById("message").value.trim();
 
         // NAME validation
         if (name === "") {
             document.getElementById("nameError").textContent =
-                "Full name is required.";
+               "Full name is required.";
+             isValid = false;
+        }else if (name.length < 3) {
+            document.getElementById("nameError").textContent =
+             "Name must contain at least 3 characters.";
             isValid = false;
         }
 
         // EMAIL validation
         if (email === "") {
+           document.getElementById("emailError").textContent =
+             "Email is required.";
+             isValid = false;
+        }else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
             document.getElementById("emailError").textContent =
-                "Email is required.";
-            isValid = false;
-        } else if (!email.includes("@") || !email.includes(".")) {
-            document.getElementById("emailError").textContent =
-                "Enter a valid email address.";
-            isValid = false;
+              "Please enter a valid email address.";
+               isValid = false;
         }
 
         // SUBJECT validation
         if (subject === "") {
             document.getElementById("subjectError").textContent =
-                "Subject is required.";
-            isValid = false;
+               "Subject is required.";
+                isValid = false;
+        }else if (subject.length < 5) {
+            document.getElementById("subjectError").textContent =
+               "Subject must contain at least 5 characters.";
+                isValid = false;
         }
 
         // MESSAGE validation
         if (message === "") {
             document.getElementById("messageError").textContent =
-                "Message is required.";
-            isValid = false;
-        } else if (message.length < 10) {
+              "Message is required.";
+               isValid = false;
+        }else if (message.length < 20) {
             document.getElementById("messageError").textContent =
-                "Message must be at least 10 characters.";
-            isValid = false;
+               "Message must contain at least 20 characters.";
+                isValid = false;
         }
 
-        // SUCCESS
-        if (isValid) {
-            alert("Message sent successfully!");
-            form.reset();
-        }
-    });
+       if (isValid) {
+
+       document.getElementById("successMessage").classList.remove("d-none");
+
+       form.reset();
+
+     setTimeout(function () {
+        document.getElementById("successMessage").classList.add("d-none");
+    }, 5000);
+
+        
+}
 
 });
+
+}); 
+   
+   
+
+
 // ===============================
 // BOOK CATEGORIES
 // ===============================
